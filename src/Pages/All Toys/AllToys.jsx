c59a4346f86b9bb025ff85bcc570bdd83dab04d7
bijoymamud <bijoymamud.09@gmail.com>
2023-05-20 10:31:56 +0600

    al toy section end

--- src/Pages/All Toys/AllToys.jsx
+++ src/Pages/All Toys/AllToys.jsx
@@ -25,14 +25,14 @@ const AllToys = () => {
           <thead>
             <tr>
               <th>
-                <label>
-                  <input type="checkbox" className="checkbox" />
-                </label>
+
               </th>
-              <th>Name</th>
-              <th>Job</th>
-              <th>Favorite Color</th>
-              <th></th>
+              <th>Saller</th>
+              <th>Toy Name</th>
+              <th>Sub Category</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Info</th>
             </tr>
           </thead>
           <tbody>
--- src/Pages/Toy/ToyRow.jsx
+++ src/Pages/Toy/ToyRow.jsx
@@ -1,40 +1,41 @@
 import React from 'react';
 
-const ToyRow = ({toy}) => {
+const ToyRow = ({ toy }) => {
+  const { customerName, toyName, category, photo, price, quantity } = toy;
   return (
-    <div>
-      {/* row 1 */}
-      <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">Hart Hagerty</div>
-              <div className="text-sm opacity-50">United States</div>
-            </div>
+
+
+    <tr>
+      <th>
+
+      </th>
+      <td>
+        <div className="flex items-start">
+          <div className="avatar">
+
           </div>
-        </td>
-        <td>
-          Zemlak, Daniel and Leannon
-          <br />
-          <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-        </td>
-        <td>Purple</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
-    </div>
-  );
-};
+          <div>
+            <div className="font-semibold">{customerName}</div>
+
+          </div>
+        </div>
+      </td>
+      <td>
+
+        {toyName}
+      </td>
+      <td>
+
+        {category}
+      </td>
+      <td>{"$ " + price}</td>
+      <td>{quantity}</td>
+      <th>
+        <button className="btn btn-ghost btn-xs">details</button>
+      </th>
+    </tr>
+
+  )
+}
 
 export default ToyRow;
